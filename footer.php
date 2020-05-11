f1626cdf2ded046ca65ecd90941600650ec912c6
饭饭 <76698242@qq.com>
2020-05-12 00:12:40 +0800

    ✨ 新增打字特效

--- footer.php
+++ footer.php
@@ -29,9 +29,9 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 <?php $this->footer(); ?>
 <script src="//static.ffis.me/javascript/jquery-2.2.4.min.js"></script>
 <script src="//static.ffis.me/javascript/bootstrap.min.js" async defer></script>
-<script src="//static.ffis.me/javascript/merge.min.js?v=2020041001"></script>
-<!--<script src="--><?php //$this->options->themeUrl('js/merge.min.js'); ?><!--"></script>-->
-<script src="//static.ffis.me/javascript/MyCustom.min.js?v=20200411"></script>
+<!--<script src="//static.ffis.me/javascript/merge.min.js?v=2020041001"></script>-->
+<script src="<?php $this->options->themeUrl('js/merge.min.js'); ?>"></script>
+<script src="//static.ffis.me/javascript/MyCustom.min.js?v=2020051101"></script>
 <!--<script src="--><?php //$this->options->themeUrl('js/MyCustom.js?v=20200409'); ?><!--"></script>-->
 </body>
 <!-- 统计代码 -->
