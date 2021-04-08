cb39f6b42412a3ad0ed6d37a279b626640c559b7
饭饭 <76698242@qq.com>
2021-04-08 13:56:42 +0800

    🔧 调整视频挂载不同设备下的显示宽高

--- header.php
+++ header.php
@@ -34,7 +34,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
     <link rel="stylesheet" href="https://static.ffis.me/stylesheet/bootstrap.min.css?v20200413"/>
     <link rel="stylesheet" href="https://static.ffis.me/stylesheet/material.min.css?v=2019123001"/>
 <!--    <link rel="stylesheet" href="--><?php //$this->options->themeUrl('css/material.min.css'); ?><!--">-->
-    <link rel="stylesheet" href="https://static.ffis.me/stylesheet/customs.min.css?v=2021040801"/>
+    <link rel="stylesheet" href="https://static.ffis.me/stylesheet/customs.min.css?v=2021040802"/>
 <!--   <link rel="stylesheet" href="--><?php //$this->options->themeUrl('css/customs.css'); ?><!--">-->
     <link rel="stylesheet" href="https://static.ffis.me/stylesheet/customs-blue.min.css?v=2020120601"/>
 <!--    <link rel="stylesheet" href="--><?php //$this->options->themeUrl('css/customs-white.css'); ?><!--">-->
