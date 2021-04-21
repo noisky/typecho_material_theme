97e33c3df2e9bb685f89050316c13f32af1b3272
饭饭 <76698242@qq.com>
2021-04-21 20:58:39 +0800

    🐛 修复底部栏a标签嵌套BUG，修改底栏的图标源为jsdelivr CDN资源

--- footer.php
+++ footer.php
@@ -26,9 +26,9 @@ ob_end_flush(); //将缓冲区输入到页面，并关闭缓存区
 		<div class="container">
 			<div class="pull-left copyright">Copyright &copy; 2013-<?php _e(date('Y')) ?>&nbsp;<?php $this->options->title(); ?></div>
 			<ul class="footer-nav pull-right">
-				<li>Powered by <a href="http://typecho.org/" rel="nofollow">Typecho))) & <a href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral" target="_blank" rel="nofollow"><img src="https://static.ffis.me/img/upyun.png" width="56" height="28" style="margin: -25px 2px"  alt="upai"/></a></a></li>
+				<li>Powered by <a href="http://typecho.org/" rel="nofollow">Typecho))) & </a><a href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral" target="_blank" rel="nofollow"><img src="https://cdn.jsdelivr.net/gh/noisky/typecho_material_theme@master/img/upyun.png" width="56" height="28" style="margin: -25px 2px"  alt="upai"/></a></li>
 <!--				<li>Optimized by <a href="https://noisky.cn/" rel="nofollow">饭饭</a></li>-->
-                <li><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41019702002679"><img src="https://static.ffis.me/beian/policebeian.png" alt="beian"/> 豫公网安备 41019702002679号</a></li>
+                <li><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41019702002679"><img src="https://cdn.jsdelivr.net/gh/noisky/typecho_material_theme@master/img/policebeian.png" alt="beian"/> 豫公网安备 41019702002679号</a></li>
                 <?php if($this->options->miibeian) : ?>
                     <li><a href="http://beian.miit.gov.cn" rel="nofollow"><?php echo $this->options->miibeian; ?></a></li>
                 <?php endif; ?>
@@ -41,15 +41,17 @@ ob_end_flush(); //将缓冲区输入到页面，并关闭缓存区
 	</div>
 </footer>
 <?php $this->footer(); ?>
+<!--加载CDN资源-->
 <!--<script src="https://static.ffis.me/javascript/jquery-2.2.4.min.js"></script>-->
-<script src="https://cdn.jsdelivr.net/gh/noisky/typecho_material_theme@master/js/jquery-2.2.4.min.js"></script>
 <!--<script src="https://static.ffis.me/javascript/bootstrap.min.js" async defer></script>-->
-<script src="https://cdn.jsdelivr.net/gh/noisky/typecho_material_theme@master/js/bootstrap.min.js" async defer></script>
 <!--<script src="https://static.ffis.me/javascript/merge.min.js?v=2020120601"></script>-->
-<script src="https://cdn.jsdelivr.net/gh/noisky/typecho_material_theme@master/js/merge.min.js?v=2021042001"></script>
-<!--<script src="--><?php //$this->options->themeUrl('js/merge.min.js'); ?><!--"></script>-->
 <!--<script src="https://static.ffis.me/javascript/MyCustom.min.js?v=2020120601"></script>-->
+<script src="https://cdn.jsdelivr.net/gh/noisky/typecho_material_theme@master/js/jquery-2.2.4.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/noisky/typecho_material_theme@master/js/bootstrap.min.js" async defer></script>
+<script src="https://cdn.jsdelivr.net/gh/noisky/typecho_material_theme@master/js/merge.min.js?v=2021042001"></script>
 <script src="https://cdn.jsdelivr.net/gh/noisky/typecho_material_theme@master/js/MyCustom.min.js?v=2021042001"></script>
+<!--加载本地资源-->
+<!--<script src="--><?php //$this->options->themeUrl('js/merge.min.js'); ?><!--"></script>-->
 <!--<script src="--><?php //$this->options->themeUrl('js/MyCustom.js'); ?><!--"></script>-->
 </body>
 <!-- 统计代码 -->
